859556d51c67dce89d1f835844a68ef04ce34e92
Sky Faber <skyf@alleninstitute.org>
2011-12-21 08:39:12 -0800

    Initial App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpMe::Application.config.secret_token = '55acebc70ccc28cc44262e4c382b468e6cfc4b32e8a4a062063c5d6605a9644ed422474f764ac610be6910ff0987efdb2e0f37003de2b802dcc1a46b4239a7f4'
